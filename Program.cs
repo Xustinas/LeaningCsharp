16235cd420a059da862966fec1439bb26e7724fb
JUSTINAS <justinasrancys@gmail.com>
2021-11-14 20:49:33 +0200

    Fibinacci exercise

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LeaningCsharp
 {
@@ -6,11 +7,38 @@ namespace LeaningCsharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test");
-            void TestFunc() 
+            Console.WriteLine("Enter numbers of fibinachi");
+            int len = Convert.ToInt32(Console.ReadLine());
+
+
+            int a = 0, b = 1, c = 0;
+
+            for (int i = 0; i < len; i++)
             {
-                Console.WriteLine("Go do smth");
+                c = a + b;
+                a = b;
+                b = c;
+                Console.WriteLine(c);
             }
+
+            // Fibonacci_Recursive(len);
+            // static void Fibonacci_Recursive(int len)
+            // {
+            //     Fibonacci_Rec_Temp(0, 1, 1, len);
+            // }
+            // static void Fibonacci_Rec_Temp(int a, int b, int counter, int len)
+            // {
+            //     if (counter <= len)
+            //     {
+            //         Console.Write("{0} ", a);
+            //         Fibonacci_Rec_Temp(b, a + b, counter + 1, len);
+            //
+            //     }
+            // }
+
         }
     }
 }
+
+
+
