50abaa6cac843a38efa57521e01843e953c7be33
JUSTINAS <justinasrancys@gmail.com>
2021-11-14 21:34:08 +0200

    Random exercise with while

--- Program.cs
+++ Program.cs
@@ -7,35 +7,40 @@ namespace LeaningCsharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter numbers of fibinachi");
-            int len = Convert.ToInt32(Console.ReadLine());
-
-
-            int a = 0, b = 1, c = 0;
-
-            for (int i = 0; i < len; i++)
+            Console.WriteLine("Please enter lucky number");
+            int lucky = Convert.ToInt32(Console.ReadLine());
+            int num = new Random().Next(0, 100);
+            int count = 0;
+            bool con = false;
+            while (lucky!=num)
             {
-                c = a + b;
-                a = b;
-                b = c;
-                Console.WriteLine(c);
+                count += 1;
+                Console.WriteLine("Total count {0}", count);
+                if (lucky<num)
+                {
+                    Console.WriteLine("Number is higher");
+                    lucky = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Do you wanna continue true/false");
+                    con = Convert.ToBoolean(Console.ReadLine());
+                    if (con=false)
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Number is lower");
+                    lucky = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Do you wanna continue true/false");
+                    con = Convert.ToBoolean(Console.ReadLine());
+                    if (con = false)
+                    {
+                        break;
+                    }
+                }
+               
             }
-
-            // Fibonacci_Recursive(len);
-            // static void Fibonacci_Recursive(int len)
-            // {
-            //     Fibonacci_Rec_Temp(0, 1, 1, len);
-            // }
-            // static void Fibonacci_Rec_Temp(int a, int b, int counter, int len)
-            // {
-            //     if (counter <= len)
-            //     {
-            //         Console.Write("{0} ", a);
-            //         Fibonacci_Rec_Temp(b, a + b, counter + 1, len);
-            //
-            //     }
-            // }
-
+            Console.WriteLine("Bingo!!!!!");
         }
     }
 }
